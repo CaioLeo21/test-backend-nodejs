9d4dad5a06519900b5c756d58b49525d027748aa
Caio Leandro <caioleandro38@gmail.com>
2021-05-27 20:20:59 -0300

    Atualização na construção das rotas

--- routes.js
+++ routes.js
@@ -1,22 +1,18 @@
 module.exports = app => {
-    app.route('/v1/register')
+    app.route('/v1/products')
         .post(app.api.register_product.register_product)
-
-    app.route('/v1/list')
         .get(app.api.list_products.list_products)
 
-    app.route('/v1/list/category/:category')
+    app.route('/v1/product/:title')
+        .put(app.api.update_product.update_product)
+        .delete(app.api.delete_product.delete_product)        
+
+    app.route('/v1/products/category/:category')
         .get(app.api.products_by_category.products_by_category)
 
-    app.route('/v1/list/title/:title')
+    app.route('/v1/products/title/:title')
         .get(app.api.products_by_title.products_by_title)
 
-    app.route('/v1/update/:title')
-        .put(app.api.update_product.update_product)
-
-    app.route('/v1/update/category/:title')
+    app.route('/v1/category/:title')
         .put(app.api.edit_category.edit_category)
-
-    app.route('/v1/delete/:title')
-        .delete(app.api.delete_product.delete_product)
 }
