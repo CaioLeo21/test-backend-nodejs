e2bac4b2f228dc240e673ba97f5caec82fe3032f
Caio Leandro <caioleandro38@gmail.com>
2021-05-27 18:45:31 -0300

    Criação da API de exclusão de produto

---
+++ api/delete_product.js
@@ -0,0 +1,24 @@
+module.exports = app => {
+    //CRIAÇÃO DA API DE EXCLUSÃO DE PRODUTO
+    const Product = app.db.product_model.product_model
+
+    const delete_product = async (req, res) => {
+        try {
+            const title = req.params.title
+            const findTitle = await Product.findOneAndDelete({ title: title })
+
+            if (findTitle) {
+                return res.status(202).send("Produto excluído com sucesso!")
+            }
+            else{
+                return res.status(400).send(`O título "${title}" não existe!`)
+            }
+        } catch (error) {
+            console.log(error)
+
+            return res.status(500).send("Produto não foi excluído!")
+        }
+    }
+
+    return { delete_product }
+}
