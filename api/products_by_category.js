75bc633fd96b6552ed9fad8843ea05b4878f28fc
Caio Leandro <caioleandro38@gmail.com>
2021-05-27 18:23:29 -0300

    Update products_by_category.js
    Criação da variável category para armazenar o parâmetro da rota da requisição

--- api/products_by_category.js
+++ api/products_by_category.js
@@ -4,11 +4,13 @@ module.exports = app => {
 
     const products_by_category = async (req, res) => {
         try {
+            const category = req.params.category
+
             //NA CONSTANTE ABAIXO, UTILIZO O REGEX PARA ENCONTRAR QUALQUER CATEGORIA SELECIONADA NO PARAMS E UTILIZO O 'OPTIONS' 'i' PARA RETORNAR O DADO INDEPENDENTE DE SER 
             //MAIÚSCULA OU MINÍSCULA
-            const filter_by_category = await Product.find({ category: { $regex: req.params.category, $options: 'i' }}, { _id: 0, __v: 0 })
+            const filter_by_category = await Product.find({ category: { $regex: category, $options: 'i' }}, { _id: 0, __v: 0 })
 
-            return res.status(200).send({ message: `Produtos filtrados pela categoria '${ req.params.category }' listados com sucesso!`, data: { filter_by_category }})
+            return res.status(200).send({ message: `Produtos filtrados pela categoria '${ category }' listados com sucesso!`, data: { filter_by_category }})
         } catch (error) {
             console.log(error)
 
